f4bead8d4160f47157f4a017f0c1524f8f848efa
elliot richardson <ear78@aol.com>
2017-12-01 00:09:03 -0600

    added more interaction and practice with state in react

--- src/friends.js
+++ src/friends.js
@@ -2,21 +2,48 @@ import React from 'react';
 import Friends2 from './Friends2';
 
 export default class Friends extends React.Component {
-    // constructor(props){
-    //     super(props);
-    //     this.setState(
-    //         {username: 'Elliot'}
-    //     )
-    // }
+    constructor(props){
+        super(props);
+        this.state = {
+            color: "I am state gray",
+            styleChange: "lightblue"
+        }
+    }
+    toggleColor(){
+        if(this.state.color === "I am state gray"){
+            this.setState({color: "I am now changed state"})
+        } else {
+            this.setState({color: "I am state gray"})
+        }
+        
+    }
     
+    changeBg(event){
+        this.setState({styleChange: event.target.value})
+    }
     
     render(){
         const name = 'Elliot';
+        const styleObj = {
+            backgroundColor: "red"
+        }
+        const styleChange = {
+            backgroundColor: this.state.styleChange
+        }
         return(
             <div>
                 <h2>Hello World!</h2>
-                <p>This is a paragraph</p>
+                //style set via js variable
+                <h2 style={styleObj}>Styling This With Js</h2>
+                //style set by css
+                <p className="pink">This is a paragraph</p>
+                // name set by js variable
                 <p>Print {name}</p>
+                //function toggleColor above
+                <p onClick={this.toggleColor.bind(this)}>Print {this.state.color}</p>
+                <h4 style={styleChange}>
+                    <input value={this.state.styleChange} onChange={this.changeBg.bind(this)}/>
+                </h4>
                 <Friends2 name="Elliot"/>
                 <Friends2 name="Johnny"/>
                 <Friends2 name="Leroy"/>
