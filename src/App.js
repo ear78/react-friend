267477130ff8e790c3ba33262a5fbcc5a3d5d3fb
elliot richardson <ear78@aol.com>
2017-12-02 00:48:22 -0600

    added input with on change event to dynamically change title

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import './App.css';
 import Friends from './friends';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: "Welcome to Elliot React"
+        }
+    }
+    
+    updateTitle(e){
+        this.setState({
+            title: e.target.value
+        })
+    }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{this.state.title}</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload. Type in input box to change title.
+          <input type="text" onChange={this.updateTitle.bind(this)} />
         </p>
         <hr />
         <Friends />
